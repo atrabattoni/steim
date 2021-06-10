0c7aee7ce1ffb861740f49a65e8ce7a545a5c55a
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-06-10 12:52:05 +0200

    Write array size into the buffer

--- steim/steim.pyx
+++ steim/steim.pyx
@@ -11,23 +11,26 @@ from csteim cimport msr_decode_steim2, msr_encode_steim2
 
 
 def encode_steim2(int32_t[:] arr):
-    cdef int32_t[:] buf = view.array(shape=(4*arr.size,), 
+    cdef int32_t[:] buf = view.array(shape=(1 + 4*arr.size,), 
                                      itemsize=sizeof(int32_t), format="i")
     cdef int32_t diff0 = 0
     cdef uint16_t byteswritten = 0
     cdef char sid 
     cdef int swapflag = 0
-    samplecount = msr_encode_steim2(&arr[0], arr.size, &buf[0], buf.size, diff0, 
+    samplecount = msr_encode_steim2(&arr[0], arr.size, &buf[1], buf.size-1, diff0, 
                                     &byteswritten, &sid, swapflag)
-    enc = buf[:byteswritten//4]
-    return samplecount, enc
+    buf[0] = samplecount                           
+    enc = buf[:1+byteswritten//4]
+    return enc
 
-def decode_steim2(int64_t samplecount, int32_t[:] enc):
+
+def decode_steim2(int32_t[:] enc):
+    samplecount = enc[0]
     cdef int32_t[:] buf = view.array(shape=(4*enc.size,),
                                      itemsize=sizeof(int32_t), format="i")
     cdef int32_t[:] dec = view.array(shape=(samplecount,),
                                      itemsize=sizeof(int32_t), format="i")
-    buf[:enc.size] = enc
+    buf[:enc.size - 1] = enc[1:]
     cdef char sid 
     cdef int swapflag = 0
     msr_decode_steim2(&buf[0], buf.size, samplecount, &dec[0], dec.size, &sid, 
--- tests/test_steim.py
+++ tests/test_steim.py
@@ -11,9 +11,10 @@ class TestEncodeDecode(unittest.TestCase):
     def test_encode_decode(self):
         n = 4096
         arr = np.rint(1000 * np.random.randn(n)).astype(np.int32)
-        samplecount, enc = encode_steim2(arr)
-        dec = decode_steim2(samplecount, enc)
+        enc = encode_steim2(arr)
+        dec = decode_steim2(enc)
         self.assertTrue(np.array_equal(arr, dec))
+
 
 if __name__ == '__main__':
     unittest.main()
