f171d32a729b8b8dc9e1fd1cfcc02d7792a53588
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-06-10 14:30:05 +0200

    Add nogil

--- steim/steim.pyx
+++ steim/steim.pyx
@@ -12,31 +12,61 @@ from csteim cimport msr_decode_steim2, msr_encode_steim2
 
 def encode_steim2(int32_t[:] arr):
 
+    # Check input size
     if arr.size > ((UINT16_MAX + 1) // sizeof(int32_t)):
         raise(MemoryError("Inputted array is to big"))
 
+    # Allocate buffer
     cdef int32_t[:] buf = view.array(shape=(1 + 4*arr.size,), 
                                      itemsize=sizeof(int32_t), format="i")
+
+    # Declare C wrappings 
+    cdef int32_t *input_ptr = &arr[0]
+    cdef int samplecount = arr.size
+    cdef int32_t *output_ptr = &buf[1],
+    cdef int outputlength = buf.size - 1
     cdef int32_t diff0 = 0
     cdef uint16_t byteswritten = 0
     cdef char sid 
     cdef int swapflag = 0
-    samplecount = msr_encode_steim2(&arr[0], arr.size, &buf[1], buf.size-1, diff0, 
-                                    &byteswritten, &sid, swapflag)
-    buf[0] = samplecount                           
+
+    # Compute
+    with nogil:
+        msr_encode_steim2(input_ptr, samplecount, output_ptr, outputlength,
+                          diff0, &byteswritten, &sid, swapflag)
+    
+    # Write header
+    buf[0] = samplecount  
+
+    # Get computed data                         
     enc = buf[:1+byteswritten//sizeof(int32_t)]
+
     return enc
 
 
 def decode_steim2(int32_t[:] enc):
-    samplecount = enc[0]
+
+    # Read header
+    cdef int64_t samplecount = enc[0]
+
+    # Allocate buffer and decoded output
     cdef int32_t[:] buf = view.array(shape=(4*enc.size,),
                                      itemsize=sizeof(int32_t), format="i")
     cdef int32_t[:] dec = view.array(shape=(samplecount,),
                                      itemsize=sizeof(int32_t), format="i")
     buf[:enc.size - 1] = enc[1:]
+
+    # Declare C wrappings 
+    cdef int32_t *input_ptr = &buf[0]
+    cdef int inputlength = buf.size
+    cdef int32_t *output_ptr = &dec[0],
+    cdef int outputlength = dec.size
     cdef char sid 
     cdef int swapflag = 0
-    msr_decode_steim2(&buf[0], buf.size, samplecount, &dec[0], dec.size, &sid, 
-                      swapflag)
+
+    # Compute
+    with nogil:
+        msr_decode_steim2(input_ptr, inputlength, samplecount, output_ptr,
+                          outputlength, &sid, swapflag)
+
     return np.asarray(dec)
